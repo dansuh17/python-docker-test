4dd85ef59c9ac67b1e5e4dc6b65f403e9c8be747
Dan Suh <kaehops@gmail.com>
2019-12-22 16:27:25 +0900

    server code

---
+++ simple_server.py
@@ -0,0 +1,10 @@
+import http.server
+import socketserver
+import librosa
+
+PORT = 8080
+handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
